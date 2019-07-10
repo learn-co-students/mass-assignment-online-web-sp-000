223321ecf0ad2fcf1e9e0394fb8690debb65d199
Connor Cowan <ccowan003@comcast.net>
2019-07-10 23:28:43 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,7 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
+  #uses iteration and the .send method to mass assign the value of each key/value pair to its associated key (i.e. method). 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
