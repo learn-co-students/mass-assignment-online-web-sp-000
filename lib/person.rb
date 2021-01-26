d1eff8476f2a0213636f86da121bf761d168c7af
Clint Richards <richardsclint17@gmail.com>
2021-01-26 00:41:17 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-  #your code here
+  def initialize(**kwargs)
+    kwargs.each_pair do |method, value|
+      create_accessors(method, value)
+      public_send("#{method}=", value)
+    end
+  end
+
+  private
+
+  def create_accessors(method, value)
+    instance_eval <<-CODE
+      def #{method}=(v)
+        @#{method} = v
+      end
+      def #{method}
+        @#{method}
+      end
+    CODE
+  end
 end
